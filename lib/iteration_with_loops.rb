b9e33470da9526cc355ec8c3c2ebc9e55a1b45cf
Christina Chianelli <cechianelli@gmail.com>
2020-05-24 05:29:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_even_values(src)
   even_array = []
   while row_counter < src.count do
     element_index = 0 
-    while element_index < src[row_counter].count do
+    while element_index < src[row_counter] % 2 == 0 do
       if src[row_counter][element_index].even?
        even_array << src[row_counter][element_index] 
       end
